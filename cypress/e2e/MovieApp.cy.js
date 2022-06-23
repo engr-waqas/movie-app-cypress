9cdcc89c33e325bda5666caff39a1b78bc42027c
engr-waqas <dev.waqastaj@gmail.com>
2022-06-23 14:00:00 +0500

    Fix formatting

--- cypress/e2e/MovieApp.cy.js
+++ cypress/e2e/MovieApp.cy.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
 
-import { Context } from "mocha";
 import { Getlocator } from "../support/page_objects/MovieApp";
 
 describe("Testing the MovieApp", () => {
@@ -9,16 +8,20 @@ describe("Testing the MovieApp", () => {
       "https://install.appcenter.ms/orgs/itops-03e0/apps/qa-interview-movie-app-1/distribution_groups/link"
     );
   });
+
   describe("Verify that user can add a movie in favorites", () => {
     it("User selects the movie from movie list", () => {
       Getlocator.SelectMovie().click();
     });
+
     it("User clicks on star icon", () => {
       Getlocator.StarIcon().click();
     });
+
     it("User navigates to Favorites tab", () => {
       Getlocator.FavoriteIcon().click();
     });
+
     it("Movie should be available in Favorites tab", () => {
       Getlocator.FavoritesPage().should("contain", "WATCHER");
     });
--- cypress/support/page_objects/MovieApp.js
+++ cypress/support/page_objects/MovieApp.js
@@ -2,12 +2,15 @@ export class MovieApp {
   SelectMovie() {
     return cy.contains('[placeholder="WATCHER"]');
   }
+
   StarIcon() {
     return cy.contains('[class="Star-Icon"]');
   }
+
   FavoriteIcon() {
     return cy.contains('[class="Favorite-Icon"]');
   }
+
   FavoritesPage() {
     return cy.contains('[class="Favorite_movies"]');
   }
